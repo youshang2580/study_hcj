12f6d54c99e4732684c5160c25192e0539e5db0c
youshang2580 <20516411+youshang2580@users.noreply.github.com>
2020-12-13 01:57:37 +0800

    Update window.js

--- window.js
+++ window.js
@@ -31,11 +31,11 @@ var num = 1;
 var img = document.getElementById("img");//获取img对象
 function fun() {
 	num++;
-	if(num > 6) {
+	if(num > 18) {
 		num = 1;
 	}
 	//执行一次更换一次图片
 	img.src = "https://恋语.xyz/home/images/鬼刀18/" + num + "_鬼刀.jpg";
 }
 //setTimeout(fun,5000);
-setInterval(fun,8000);
+setInterval(fun,10000);
