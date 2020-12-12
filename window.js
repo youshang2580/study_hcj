02fb7b9f30934ac8a8744690bba5285662dd95ca
youshang2580 <20516411+youshang2580@users.noreply.github.com>
2020-12-13 01:17:26 +0800

    Add files via upload

---
+++ window.js
@@ -0,0 +1,40 @@
+/*
+    window对象
+	    直接使用，可省略window.
+*/
+//属性
+
+/*方法*/
+// 提示框相关方法
+//alert("弹出警告框");
+//confirm("确认取消提示框");
+
+// 窗口相关的方法
+//打开新窗口
+var url1 = "https://baidu.com";
+var newWindow;
+document.getElementById("open").onclick = function() {
+	newWindow = open(url1);
+};
+
+
+//关闭窗口
+document.getElementById("close").onclick = function() {
+	newWindow = close();
+};
+
+
+//定时器
+//轮播图
+var num = 1;
+var img = document.getElementById("img");//获取img对象
+function fun() {
+	num++;
+	if(num > 6) {
+		num = 1;
+	}
+	//执行一次更换一次图片
+	img.src = "/storage/emulated/0/AppProjects/MyWebsite/js/img/huyy (" + num + ").jpg";
+}
+//setTimeout(fun,5000);
+setInterval(fun,3000);
